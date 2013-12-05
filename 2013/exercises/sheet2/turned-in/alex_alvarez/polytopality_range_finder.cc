6e42a1393ec710161fa281b32405b85001c94db9
Alex Alvarez <alex.alvarez.ruiz@gmail.com>
2013-12-05 02:40:16 +0100

    Changes in the polytopality range finder code

--- 2013/exercises/sheet2/turned-in/alex_alvarez/polytopality_range_finder.cc
+++ 2013/exercises/sheet2/turned-in/alex_alvarez/polytopality_range_finder.cc
@@ -9,7 +9,7 @@ using namespace boost;
 typedef vector<int> VI;
 typedef vector<VI> VVI;
 typedef pair<int, int> PII;
-typedef adjacency_list<vecS, vecS, undirectedS, property<vertex_index_t, int> > graph_boost;
+typedef adjacency_list<vecS, vecS, undirectedS, property<vertex_index_t, int> > boost_graph;
 
 //Adjacency list of the graph
 VVI adj;
@@ -17,7 +17,7 @@ VVI adj;
 //Checks planarity of the graph using Boost's implementation of Boyer-Myrvold's algorithm
 bool check_planarity() {
   int n = adj.size();
-  graph_boost G(n);
+  boost_graph G(n);
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < int(adj[i].size()); ++j) {
       int v = adj[i][j];
@@ -30,13 +30,13 @@ bool check_planarity() {
 //Generates the graph of n vertices and S as subset of differences
 void generate_graph(int n, const VI& S) {
   adj = VVI(n);
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i + 1 < n; ++i) {
     for (int j = i + 1; j < n; ++j) {
       int dif = j - i, dif2 = i - j + n;
-      bool ok = false;
-      for (int k = 0; k < int(S.size()) and not ok; ++k)
-        if (S[k] == dif or S[k] == dif2) ok = true;
-      if (ok) {
+      bool valid_edge = false;
+      for (int k = 0; k < int(S.size()) and not valid_edge; ++k)
+        if (S[k] == dif or S[k] == dif2) valid_edge = true;
+      if (valid_edge) {
         adj[i].push_back(j);
         adj[j].push_back(i);
       }
@@ -55,6 +55,16 @@ int dfs(int u, vector<bool>& seen, int mask) {
   return res;
 }
 
+//Return the number of bits in x set to one
+int set_bits(int x) {
+  int res = 0;
+  while (x > 0) {
+    if (x & 1) ++res;
+    x >>= 1;
+  }
+  return res;
+}
+
 //Checks Balinski's theorem for dimension d
 bool check_balinski(int d) {
   int n = adj.size();
@@ -63,7 +73,7 @@ bool check_balinski(int d) {
   //over every (d-1)-subset of the nodes
   for (int mask = 0; mask < (1 << n); ++mask) {
     //The bits sets to one codify the cut-set
-    if (__builtin_popcount(mask) == d - 1) {
+    if (set_bits(mask) == d - 1) {
       vector<bool> seen(n, false);
       
       //Select a vertex that is not in the candidate cut
@@ -78,21 +88,22 @@ bool check_balinski(int d) {
 }
 
 //Auxiliar recursive function for the PSP checker
-bool rec(int act, int u, set<int>& usat, vector<PII>& connections) {
+//Backtracks to find a principal subdivision of the complete graph
+bool rec(int act, int u, set<int>& used, vector<PII>& connections) {
   if (u == connections[act].second) {
     if (act + 1 == connections.size()) return true;
-    return rec(act + 1, connections[act + 1].first, usat, connections);
+    return rec(act + 1, connections[act + 1].first, used, connections);
   }
-  bool ok = false;
-  for (int i = 0; i < adj[u].size() and not ok; ++i) {
+  bool found = false;
+  for (int i = 0; i < adj[u].size() and not found; ++i) {
     int v = adj[u][i];
-    if (usat.count(v) and connections[act].second != v) continue;
-    usat.insert(v);
+    if (used.count(v) and connections[act].second != v) continue;
+    used.insert(v);
 
-    if (rec(act, v, usat, connections)) ok = true;
-    if (connections[act].second != v) usat.erase(v);
+    if (rec(act, v, used, connections)) found = true;
+    if (connections[act].second != v) used.erase(v);
   }
-  return ok;
+  return found;
 }
 
 //Checks with a backtracking if there is a principal subdivision of K_{d + 1}
@@ -100,40 +111,48 @@ bool psp(int d) {
   if (adj[0].size() < d) return false;
   vector<int> vertices;
   for (int i = 0; i < d; ++i) vertices.push_back(adj[0][i]);
-  set<int> usat;
+  set<int> used;
   
-  usat.insert(0);
-  for (int i = 0; i < d; ++i) usat.insert(adj[0][i]);
+  used.insert(0);
+  used.insert(adj[0].begin(), adj[0].begin() + d);
   vector<PII> connections;
-  for (int i = 0; i < d; ++i) {
+  for (int i = 0; i + 1 < d; ++i) {
     for (int j = i + 1; j < d; ++j) {
-      bool ok = false;
+      bool already_connected = false;
       for (int k = 0; k < adj[vertices[i]].size(); ++k) {
         if (adj[vertices[i]][k] == vertices[j]) {
-          ok = true;          
+          already_connected = true;          
         }
       }
-      if (not ok) connections.push_back(PII(vertices[i], vertices[j]));
+      if (not already_connected) connections.push_back(PII(vertices[i], vertices[j]));
     }
   }
   
-  if (connections.size() == 0) return true;
-  return rec(0, connections[0].first, usat, connections);
+  return connections.size() == 0 ? true : rec(0, connections[0].first, used, connections);
 }
 
 
-int main() {
+int main(int argc, char** argv) {
   //Number of nodes
   int n;
-  cin >> n;
   
   //Size of S
   int s_size;
-  cin >> s_size;
+  
+  if (argc < 3 or argc < atoi(argv[2]) + 3) {
+    cout << "usage: program n k s1 ... sk" << endl;
+    cout << "\t\tn: the number of nodes" << endl;
+    cout << "\t\tk: the number of differences that define the circulant graph" << endl;
+    cout << "\t\tsi: the i-th difference" << endl;
+    return 1;
+  }
+  
+  n = atoi(argv[1]);
+  s_size = atoi(argv[2]);
   
   VI S(s_size);
   for (int i = 0; i < s_size; ++i) 
-    cin >> S[i];
+    S[i] = atoi(argv[2 + i]);
   
   //Generate the graph
   generate_graph(n, S);
@@ -144,6 +163,7 @@ int main() {
     cerr << endl;
   }
   
+  //candidate[i] = true iff i is a valid dimension so far
   vector<bool> candidate(n, true);
   candidate[0] = candidate[1] = false;
   
