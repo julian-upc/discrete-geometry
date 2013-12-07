c353edba96156b5202ce87241a56b061873f2b6c
Alex Alvarez <alex.alvarez.ruiz@gmail.com>
2013-12-07 17:47:28 +0100

    More changes in the polytopality range finder code

--- 2013/exercises/sheet2/turned-in/alex_alvarez/polytopality_range_finder.cc
+++ 2013/exercises/sheet2/turned-in/alex_alvarez/polytopality_range_finder.cc
@@ -11,16 +11,13 @@ typedef vector<VI> VVI;
 typedef pair<int, int> PII;
 typedef adjacency_list<vecS, vecS, undirectedS, property<vertex_index_t, int> > boost_graph;
 
-//Adjacency list of the graph
-VVI adj;
-
 //Checks planarity of the graph using Boost's implementation of Boyer-Myrvold's algorithm
-bool check_planarity() {
+bool check_planarity(VVI& adj) {
   int n = adj.size();
   boost_graph G(n);
   for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < int(adj[i].size()); ++j) {
-      int v = adj[i][j];
+    for (size_t j = 0; j < adj[i].size(); ++j) {
+      const int v = adj[i][j];
       if (i < v) add_edge(i, v, G);
     }
   }
@@ -28,14 +25,14 @@ bool check_planarity() {
 }
 
 //Generates the graph of n vertices and S as subset of differences
-void generate_graph(int n, const VI& S) {
+void generate_graph(int n, const VI& S, VVI& adj) {
   adj = VVI(n);
   for (int i = 0; i + 1 < n; ++i) {
     for (int j = i + 1; j < n; ++j) {
       int dif = j - i, dif2 = i - j + n;
       bool valid_edge = false;
-      for (int k = 0; k < int(S.size()) and not valid_edge; ++k)
-        if (S[k] == dif or S[k] == dif2) valid_edge = true;
+      for (VI::const_iterator it = S.begin(); it != S.end() and not valid_edge; ++it)
+        if (*it == dif or *it == dif2) valid_edge = true;
       if (valid_edge) {
         adj[i].push_back(j);
         adj[j].push_back(i);
@@ -45,18 +42,18 @@ void generate_graph(int n, const VI& S) {
 }
 
 //A simple DFS to count the reachable nodes
-int dfs(int u, vector<bool>& seen, int mask) {
+int dfs(int u, vector<bool>& seen, int mask, VVI& adj) {
   seen[u] = true;
   int res = 1;
   for (int i = 0; i < int(adj[u].size()); ++i) {
     int v = adj[u][i];
-    if (not seen[v] and ((mask >> v)&1) == 0) res += dfs(v, seen, mask);
+    if (not seen[v] and ((mask >> v)&1) == 0) res += dfs(v, seen, mask, adj);
   }
   return res;
 }
 
 //Return the number of bits in x set to one
-int set_bits(int x) {
+int bits_set(int x) {
   int res = 0;
   while (x > 0) {
     if (x & 1) ++res;
@@ -66,14 +63,14 @@ int set_bits(int x) {
 }
 
 //Checks Balinski's theorem for dimension d
-bool check_balinski(int d) {
+bool check_balinski(int d, VVI& adj) {
   int n = adj.size();
   
   //Iterate (in a very inefficient way but fast to code way)
   //over every (d-1)-subset of the nodes
   for (int mask = 0; mask < (1 << n); ++mask) {
     //The bits sets to one codify the cut-set
-    if (set_bits(mask) == d - 1) {
+    if (bits_set(mask) == d - 1) {
       vector<bool> seen(n, false);
       
       //Select a vertex that is not in the candidate cut
@@ -81,7 +78,7 @@ bool check_balinski(int d) {
       while ((mask >> start_vertex)&1) ++start_vertex;
       
       //DFS to check whether there's more than one component
-      if (dfs(start_vertex, seen, mask) != n - d + 1) return false;
+      if (dfs(start_vertex, seen, mask, adj) != n - d + 1) return false;
     }
   }
   return true;
@@ -89,25 +86,25 @@ bool check_balinski(int d) {
 
 //Auxiliar recursive function for the PSP checker
 //Backtracks to find a principal subdivision of the complete graph
-bool rec(int act, int u, set<int>& used, vector<PII>& connections) {
+bool rec(int act, int u, set<int>& used, vector<PII>& connections, VVI& adj) {
   if (u == connections[act].second) {
     if (act + 1 == connections.size()) return true;
-    return rec(act + 1, connections[act + 1].first, used, connections);
+    return rec(act + 1, connections[act + 1].first, used, connections, adj);
   }
   bool found = false;
-  for (int i = 0; i < adj[u].size() and not found; ++i) {
-    int v = adj[u][i];
+  for (size_t i = 0; i < adj[u].size() and not found; ++i) {
+    const int v = adj[u][i];
     if (used.count(v) and connections[act].second != v) continue;
     used.insert(v);
 
-    if (rec(act, v, used, connections)) found = true;
+    if (rec(act, v, used, connections, adj)) found = true;
     if (connections[act].second != v) used.erase(v);
   }
   return found;
 }
 
 //Checks with a backtracking if there is a principal subdivision of K_{d + 1}
-bool psp(int d) {
+bool psp(int d, VVI& adj) {
   if (adj[0].size() < d) return false;
   vector<int> vertices;
   for (int i = 0; i < d; ++i) vertices.push_back(adj[0][i]);
@@ -119,7 +116,7 @@ bool psp(int d) {
   for (int i = 0; i + 1 < d; ++i) {
     for (int j = i + 1; j < d; ++j) {
       bool already_connected = false;
-      for (int k = 0; k < adj[vertices[i]].size(); ++k) {
+      for (size_t k = 0; k < adj[vertices[i]].size(); ++k) {
         if (adj[vertices[i]][k] == vertices[j]) {
           already_connected = true;          
         }
@@ -128,7 +125,7 @@ bool psp(int d) {
     }
   }
   
-  return connections.size() == 0 ? true : rec(0, connections[0].first, used, connections);
+  return connections.size() == 0 ? true : rec(0, connections[0].first, used, connections, adj);
 }
 
 
@@ -155,11 +152,12 @@ int main(int argc, char** argv) {
     S[i] = atoi(argv[2 + i]);
   
   //Generate the graph
-  generate_graph(n, S);
+  VVI adj;
+  generate_graph(n, S, adj);
   
   for (int i = 0; i < n; ++i) {
     cerr << i << ": ";
-    for (int j = 0; j < adj[i].size(); ++j) cerr << adj[i][j] << " ";
+    for (size_t j = 0; j < adj[i].size(); ++j) cerr << adj[i][j] << " ";
     cerr << endl;
   }
   
@@ -175,12 +173,12 @@ int main(int argc, char** argv) {
   
   //Check in a naive way Balinski theorem
   for (int i = 2; i < n; ++i) {
-    if (candidate[i]) candidate[i] = check_balinski(i);    
+    if (candidate[i]) candidate[i] = check_balinski(i, adj);    
   }
   
-  for (int i = 4; i < n; ++i) if (candidate[i]) candidate[i] = psp(i);
+  for (int i = 4; i < n; ++i) if (candidate[i]) candidate[i] = psp(i, adj);
   
-  if (candidate[3]) candidate[3] = check_planarity();
+  if (candidate[3]) candidate[3] = check_planarity(adj);
     
   cout << "Valid candidates so far:";
   for (int i = 0; i < n; ++i) {
